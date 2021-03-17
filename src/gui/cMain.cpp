1a05a1b1ac91bffdd9df9ca188960a5abae49835
RuiDGPires <pires.rui@outlook.pt>
2021-03-17 22:03:21 +0000

    Main funcionality is working now

--- src/gui/cMain.cpp
+++ src/gui/cMain.cpp
@@ -2,33 +2,65 @@
 
 #include <iostream>
 
-enum{BTN, TXTBOX, LABEL};
+enum{BTN_LOAD, BTN_RUN, BTN_STEP, TXTBOX, LABEL};
+enum{MENU_OPEN};
 
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
-    EVT_BUTTON(BTN, cMain::OnButtonClicked)
+    EVT_BUTTON(BTN_RUN, cMain::OnButtonRunClicked)
+    EVT_BUTTON(BTN_STEP, cMain::OnButtonStepClicked)
+    EVT_BUTTON(BTN_LOAD, cMain::OnButtonLoadClicked)
+    EVT_MENU(MENU_OPEN, cMain::OnMenuOpenClicked)
 wxEND_EVENT_TABLE()
 
 
 cMain::cMain() :wxFrame(nullptr, wxID_ANY, "Turing Machine Emulator", wxDefaultPosition, wxDefaultSize){
     evl::Evaluator ev(&tm);
     
-    if (!ev.evalFile("example.txt"))
-        exit(1);
+    //if (!ev.evalFile("example.txt"))
+        //exit(1);
     
     this->SetMinSize(wxSize(640,480));
     
-    btn = new wxButton(this, BTN, "Button", wxPoint(10,10), wxSize(150,50));
-    txtbox_input = new wxTextCtrl(this, TXTBOX,"", wxPoint(10,80), wxSize(200,10));
+    /* Buttons */
+    btn_load = new wxButton(this, BTN_LOAD, "Load", wxPoint(10,10), wxSize(100,30));
+    btn_run = new wxButton(this, BTN_RUN, "Run", wxPoint(120,10), wxSize(100,30));
+    btn_step = new wxButton(this, BTN_STEP, "Step", wxPoint(240,10), wxSize(100,30));
     
+    txtbox_input = new wxTextCtrl(this, TXTBOX,"", wxPoint(10,80), wxSize(200,10));
     label1 = new wxStaticText(this, LABEL, "Output", wxPoint(10,120), wxSize(600,15));
+
+    /* Menu */
+    menu_bar = new wxMenuBar();
+    this->SetMenuBar(menu_bar);
+
+    wxMenu *menuFile = new wxMenu();
+    menuFile->Append(MENU_OPEN, "Open");
+
+    menu_bar->Append(menuFile,"File");
 }
 
 cMain::~cMain(){}
 
-
-void cMain::OnButtonClicked(wxCommandEvent &evt){
+void cMain::OnButtonLoadClicked(wxCommandEvent &evt){
     tm.load(std::string(txtbox_input->GetValue().mb_str()));
+    label1->SetLabelText(tm.toString());
+}
+
+void cMain::OnButtonRunClicked(wxCommandEvent &evt){
     tm.run();
     label1->SetLabelText(tm.toString());
+}
+
+void cMain::OnButtonStepClicked(wxCommandEvent &evt){
+    tm.step();
+    label1->SetLabelText(tm.toString());
+}
+
+void cMain::OnMenuOpenClicked(wxCommandEvent &evt){
+    wxFileDialog dlg(this, "Open config file", "", "", ".txt files (*.txt)|*.txt| .conf files (*.conf)|*.conf", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+    if (dlg.ShowModal() == wxID_OK){
+        evl::Evaluator ev(&tm);
+        ev.evalFile(std::string(dlg.GetPath().c_str())); 
+    }
 }
--- src/gui/include/cMain.hpp
+++ src/gui/include/cMain.hpp
@@ -9,18 +9,29 @@
     #include <wx/wx.h>
 #endif
 
-
 class cMain : public wxFrame{
     private:
         tmch::TuringMachine tm;
     public:
         cMain();
         ~cMain();
-        void OnButtonClicked(wxCommandEvent &evt);
+
+        /* Events */
+        void OnButtonRunClicked(wxCommandEvent &evt);
+        void OnButtonStepClicked(wxCommandEvent &evt);
+        void OnButtonLoadClicked(wxCommandEvent &evt);
+
+        void OnMenuOpenClicked(wxCommandEvent &evt);
+        
         wxDECLARE_EVENT_TABLE();
     public:
         wxTextCtrl *txtbox_input;
-        wxButton *btn;
+
+        /* Buttons */
+        wxButton *btn_run;
+        wxButton *btn_step;
+        wxButton *btn_load;
+
         wxMenuBar *menu_bar;
         wxStaticText *label1;
         
