5ea22bbe6c18bbc9a87987f7268cdf10415ecc3d
RuiDGPires <pires.rui@outlook.pt>
2021-03-17 15:52:14 +0000

    Working on gui

--- src/gui/cMain.cpp
+++ src/gui/cMain.cpp
@@ -1,16 +1,34 @@
 #include "include/cMain.hpp"
 
-cMain::cMain() :wxFrame(nullptr, wxID_ANY, "Turing Machine Emulator", wxDefaultPosition, wxDefaultSize){
-    this->SetMinSize(wxSize(640,480));
-    txtbox_input = new wxTextCtrl(this, wxID_ANY,"", wxPoint(10,10), wxSize(200,10));
-    menu_bar = new wxMenuBar();
+#include <iostream>
 
-    wxMenu *menu_file = new wxMenu();
-    menu_file->Append(1001, "New");
-    menu_file->Append(1001, "Open");
-    menu_file->Append(1001, "Exit");
-    menu_bar->Append(menu_file, "File");
+enum{BTN, TXTBOX, LABEL};
+
+
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+    EVT_BUTTON(BTN, cMain::OnButtonClicked)
+wxEND_EVENT_TABLE()
+
+
+cMain::cMain() :wxFrame(nullptr, wxID_ANY, "Turing Machine Emulator", wxDefaultPosition, wxDefaultSize){
+    evl::Evaluator ev(&tm);
     
+    if (!ev.evalFile("example.txt"))
+        exit(1);
+    
+    this->SetMinSize(wxSize(640,480));
+    
+    btn = new wxButton(this, BTN, "Button", wxPoint(10,10), wxSize(150,50));
+    txtbox_input = new wxTextCtrl(this, TXTBOX,"", wxPoint(10,80), wxSize(200,10));
+    
+    label1 = new wxStaticText(this, LABEL, "Output", wxPoint(10,120), wxSize(600,15));
 }
 
 cMain::~cMain(){}
+
+
+void cMain::OnButtonClicked(wxCommandEvent &evt){
+    tm.load(std::string(txtbox_input->GetValue().mb_str()));
+    tm.run();
+    label1->SetLabelText(tm.toString());
+}
--- src/gui/include/cMain.hpp
+++ src/gui/include/cMain.hpp
@@ -1,18 +1,29 @@
 #ifndef __CMAIN_HPP__
 #define __CMAIN_HPP__
 
+#include "../../turingmachine/include/turingmachine.hpp"
+#include "../../evaluator/include/evaluator.hpp"
+
+
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
 
 
 class cMain : public wxFrame{
+    private:
+        tmch::TuringMachine tm;
     public:
         cMain();
         ~cMain();
+        void OnButtonClicked(wxCommandEvent &evt);
+        wxDECLARE_EVENT_TABLE();
     public:
         wxTextCtrl *txtbox_input;
+        wxButton *btn;
         wxMenuBar *menu_bar;
+        wxStaticText *label1;
+        
 };
 
 #endif
